10dbb610abfa01841fc0ac47405e77b7e15d3ca0
Scott Colson <scott>
2020-12-29 14:14:56 -0800

    Add SearchDateFields filter

--- CCLLC.CDS.Sdk.Data.UnitTests/SearchFilterTests.cs
+++ CCLLC.CDS.Sdk.Data.UnitTests/SearchFilterTests.cs
@@ -23,8 +23,38 @@ namespace CCLLC.CDS.Sdk.Data.UnitTest
                 var qryExpression = new QueryExpressionBuilder<Account>()                    
                        .WhereAll(a => a
                             .IsActive()
-                            .WithSearchFields<Account>(cols => new { cols.Name }))                            
+                            .WithSearchFields<Account>(cols => new { cols.Name }))
                        .WithSearchValue("mytestvalue")
+                       .Build();
+
+                var criteria = qryExpression.Criteria;
+
+                Assert.AreEqual(LogicalOperator.And, criteria.FilterOperator);
+                Assert.AreEqual(1, criteria.Filters.Count);
+                Assert.AreEqual(1, criteria.Conditions.Count);
+
+                var searchFilter = criteria.Filters[0];
+                Assert.AreEqual(LogicalOperator.Or, searchFilter.FilterOperator);
+                Assert.AreEqual(1, searchFilter.Conditions.Count);
+            }
+        }
+
+
+        [TestMethod]
+        public void Test_WithDateSearchFields_Should_CreateOrFilter()
+        {
+            new WithDateSearchFields_Should_CreateOrFilter().Test();
+        }
+
+        private class WithDateSearchFields_Should_CreateOrFilter : TestMethodClassBase
+        {
+            protected override void Test(IOrganizationService service)
+            {
+                var qryExpression = new QueryExpressionBuilder<Account>()
+                       .WhereAll(a => a
+                            .IsActive()
+                            .WithDateSearchFields<Account>(cols => new { cols.CreatedOn }))
+                       .WithSearchValue("11/1/2020")
                        .Build();
 
                 var criteria = qryExpression.Criteria;
